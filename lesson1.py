0e9c519588fcc3993b3e069e077f03191bf87e3e
Asab1984 <oleksandr.sabaydash@gmail.com>
2023-06-03 14:48:58 +0300

    first variant

---
+++ lesson1.py
@@ -0,0 +1,25 @@
+a = input('Введіть перше число')
+b = input('Введіть друге число')
+c = input('Введіть третє число')
+res1 = int(a) + int(b) + int(c)
+res2 = int(a) * int(b) * int(c)
+print('Сума чисел:', res1 , '\nДобуток чисел:' , res2)
+
+print('\n')
+zp=input('Введіть суму з/п')
+pl=input('Введіть суму місячного платежу за кредитом банку')
+kr=input('Введіть заборгованість за комунальні послуги')
+zlsh = float(zp)-float(pl)-float(kr)
+print('Залишок:', zlsh)
+
+print('\n')
+d1=input('Введіть довжину 1-ї діагоналі ')
+d2=input('Введіть довжину 2-ї діагоналі ')
+sR=float(d1)*float(d2)/2
+print('Площа ромба = ', sR)
+
+print('\n')
+print('To be \nor not \nto be')
+
+print('\n')
+print('"«Life is what happens \n\t\twhen \n\t\t\tyou’re busy making other plans"\n\t\t\t\t\t\t\t\tJohn Lennon')
